3dd4d3dfdc2707d8f6e45d9d04dd2466bf391ad3
Mehmet Emin Aruk <byemin00@gmail.com>
2023-10-29 12:54:31 +0300

    exercies new update

--- numpy-notes/sample.py
+++ numpy-notes/sample.py
@@ -1,12 +1,18 @@
-i = 0
-number = 16
+# i = 0
+# number = 16
 
-while True:
+# while True:
     
-    number = number / 2
+#     number = number / 2
 
-    i += 1
-    if number == 1:
-        break
+#     i += 1
+#     if number == 1:
+#         break
 
-print(i)
+# print(i)
+
+# liste = [1,2,3,4,5,6,7,9]
+# print(sum(liste, 3))
+
+import numpy as np
+print(np.sum(range(5), -1))
